54d7aca0f9dce4c328b59a1e746450de1da214fc
Maxim Komarov <komarovmaxim90@gmail.com>
2025-04-25 18:10:42 +0300

    Fix

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,14 +1,42 @@
 function addEvent() {
   const date = document.getElementById("datePicker").value;
   const description = document.getElementById("eventDescription").value;
-  if (date && description) {
-    const eventList = document.getElementById("eventList");
-    const eventItem = document.createElement("div");
-    eventItem.className = "event";
-    eventItem.innerHTML = `<strong>${date}</strong>: ${description}`;
-    eventList.appendChild(eventItem);
-    document.getElementById("eventDescription").value = "";
-  } else {
-    alert("Пожалуйста, выберите дату и введите описание события.");
+
+  if (!description) {
+    alert("Пожалуйста, введите описание события.");
+    return;
   }
+
+  if (description.trim().length > 2000 || description.trim().length <= 0) {
+    alert("Описание события не должно превышать 2000 символов, и не может состоять из пробельных символов.");
+    return;
+  }
+
+  if (!date) {
+    alert("Пожалуйста, введите корректную дату.");
+    return;
+  }
+
+  const selectedDate = new Date(date);
+  const year = selectedDate.getFullYear();
+
+  if (year < 2000 || year > 2100) {
+    alert("Год должен быть в диапазоне от 2000 до 2100.");
+    return;
+  }
+
+  const dateString = selectedDate.toISOString().split('T')[0];
+  if (dateString !== date) {
+    alert("Пожалуйста, введите корректную дату.");
+    return; 
+  }
+
+  const eventList = document.getElementById("eventList");
+  const eventItem = document.createElement("div");
+  eventItem.className = "event";
+  eventItem.innerHTML = `<strong>${date}</strong>: ${description}`;
+  eventList.appendChild(eventItem);
+
+  document.getElementById("eventDescription").value = "";
+  document.getElementById("datePicker").value = "";
 }
